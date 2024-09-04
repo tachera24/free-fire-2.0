9ab33b8dbf8316391da8b3798472d53e7f95e9fc
tachera24 <lukaspedro383@gmail.com>
2024-09-04 07:17:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ avanca.forEach(button => {
         const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
         atual.classList.remove('ativo');
-        document.getElementById(proximoPasso).classList.add('ativo');
+        document.getElementById(proximoPasso).classList.add('passivo');
     })
 })
